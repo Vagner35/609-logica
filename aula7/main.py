d9eb4a6066160d3a36b883c04c8868bb24301ae2
Vagner35 <148567373+Vagner35@users.noreply.github.com>
2023-12-01 12:22:20 -0300

    Add files via upload

---
+++ aula7/main.py
@@ -0,0 +1,7 @@
+gato={'nome':str(input('digite o nome do gato')),
+      'cor':str(input('digite a cor do gato')),
+      'raça':str(input('digite a raça do gato')),
+      'adestrado':bool(input('digite se ele é adestrado')),
+      'idade':float(input('digite a idade do gato'))}
+print(gato)
+
