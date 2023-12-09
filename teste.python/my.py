39e65cfe262b7133ecdf0411840b762429338c06
Vagner35 <148567373+Vagner35@users.noreply.github.com>
2023-12-09 11:18:45 -0300

    Add files via upload

---
+++ teste.python/my.py
@@ -0,0 +1,16 @@
+#Tipos de variáveis:
+nome='João'
+idade= 25
+altura= 1.75
+cadastrado= True
+
+
+
+
+#Dados pessoais
+usuario=str(input('Digite um nome qualquer:'))
+idade= int(input('Digite sua idade:'))
+altura=float(input('Digite sua altura:'))
+endereço= str(input('Digite seu endereço:'))
+
+print(f'Me chamo {usuario}, tenho {idade} anos, minha altura é {altura}, e moro na rua {endereço}.')
